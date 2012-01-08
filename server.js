1b86aa1121d64ba944e1db206a2a5e6fed541f92
Eóin Martin <eoin.martin.3007@gmail.com>
2012-01-08 18:31:23 +0000

    ooops

--- server.js
+++ server.js
@@ -294,7 +294,7 @@ function getUserLevel(userID)
 function getUsername(userID)
 {
 	SQLclient.query(
-  						'SELECT `name` FROM '+EMPLOYEE_TABLE+' WHERE `id` = '+userID+,
+  						'SELECT `name` FROM '+EMPLOYEE_TABLE+' WHERE `id` = '+userID ,
   						function selectCb(err, results, fields) {
     					if (err) {
       					throw err;
